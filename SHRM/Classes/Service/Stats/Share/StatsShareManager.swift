acde42b4093bc049792cae4680d9ef50c625e0ea
Sergei Pankov <sergeipankov@Sergeis-MacBook-Air.local>
2022-04-12 14:05:36 +0100

    config

--- SHRM/Classes/Service/Stats/Share/StatsShareManager.swift
+++ SHRM/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.shrm.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.shrm.widget")
     }
     
     @discardableResult
